20c8598efd160aaa784a442f4a8dace527c5fccd
kamcio819 <kamcio819@gmail.com>
2018-07-11 17:38:07 +0200

    Added new ships

---
+++ Assets/Scripts/Ships/BattleShip.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BattleShipData))]
+public class BattleShip : MonoBehaviour {
+
+	[SerializeField]	
+	public BattleShipData shipData;
+}
---
+++ Assets/Scripts/Ships/BattleShipData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+#region ShipType
+	public enum BattleShipType {
+		TILE1,
+		TILE2,
+		TILE3,
+		TILE4,
+		TILE6,
+		TILE7,
+		TILE8
+	}
+
+#endregion
+
+[System.Serializable]
+public struct BattleShipData {
+
+	[SerializeField]
+	private BattleShipType typeOfShip;
+} 
+
+
