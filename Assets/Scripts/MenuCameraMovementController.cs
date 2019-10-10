ff766a35a2a88f401013cd468d93b4bbefe10021
kamcio819 <kamcio819@gmail.com>
2019-10-10 14:28:55 +0200

    Prepared prototype menu

---
+++ Assets/Scripts/MenuCameraMovementController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Camera))]
+public class MenuCameraMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraTransform;
+
+    [SerializeField]
+    private Transform centerOfRotation;
+
+    [SerializeField]
+    [Range(1, 10f)]
+    private float angleRotation = 5f;
+
+    [SerializeField]
+    [Range(1,5)]
+    private float speedModifier = 1f;
+
+    private Vector3 centerOfRotationPosition;
+
+    private void Awake() {
+        centerOfRotationPosition = centerOfRotation.position;
+    }
+
+    private void Update() {
+        cameraTransform.RotateAround(centerOfRotationPosition, Vector3.up, angleRotation * Time.deltaTime * speedModifier);
+    }
+}
