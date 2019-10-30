300d99e40803fe4f8903b0e942131348996ff57f
kamcio819 <kamcio819@gmail.com>
2019-10-30 20:33:16 +0100

    Added MainMenu implementation

---
+++ Assets/SettingsMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenuController : UIController
+{
+    [SerializeField]
+    private Transform openedPanelParent;
+
+    [SerializeField]
+    private Transform disabledPanelParent;
+
+    public override void OpenScreen(string screenName)
+    {
+        for(int i = 0; i < menuScreens.Count; ++i)
+        {
+            menuScreens[i].transform.SetParent(disabledPanelParent);
+        }
+        activeScreen = menuScreens.Find(x => x.name == screenName);
+        activeScreen.transform.SetParent(openedPanelParent);
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIController : MonoBehaviour
+{
+    [SerializeField]
+    protected List<GameObject> menuScreens;
+
+    protected GameObject activeScreen;
+
+    public virtual void OpenScreen(string screenName)
+    {
+        CloseScreens();
+        activeScreen = menuScreens.Find(x => x.name == screenName);
+        activeScreen.SetActive(true);
+    }
+
+    private void CloseScreens()
+    {
+        for(int i =0; i < menuScreens.Count; ++i)
+        {
+            menuScreens[i].SetActive(false);
+        }
+    }
+
+    public virtual void CloseScreen(string screenName)
+    {
+        GameObject screen = menuScreens.Find(x => x.name == screenName);
+        screen.SetActive(false);
+        activeScreen = null;
+    }
+}
