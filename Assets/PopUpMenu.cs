fed836551d82b3893fc85e3e9d22b0bd787d27a1
kamcio819 <kamcio819@gmail.com>
2019-11-03 14:43:58 +0100

    Added DOTween, added Game Menu UI implementation

---
+++ Assets/PopUpMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class PopUpMenu : MonoBehaviour
+{
+    [SerializeField]
+    private bool active = false;
+
+    [SerializeField]
+    private RectTransform rectTransform;
+
+    [SerializeField]
+    private float x;
+
+    public void TogglePopUpMenu()
+    {
+        if(active)
+        {
+            rectTransform.DOAnchorPos(new Vector3(-900f, 0f, 0f), 0.3f);
+            active = false;
+        }
+        else
+        {
+            rectTransform.DOAnchorPos(new Vector3(x, 0f, 0f), 0.3f);
+            active = true;
+        }
+    }
+
+    public void HidePopUpMenu()
+    {
+        rectTransform.DOAnchorPos(new Vector3(-900f, 0f, 0f), 0.3f);
+        active = false;
+    }
+}
---
+++ Assets/UIAnimationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIAnimationController : MonoBehaviour
+{
+    private enum GameUI
+    {
+        PauseMenu,
+        BuildingsMenu,
+        UpgradeMenu,
+        ShipsMenu
+    }
+
+    [SerializeField]
+    private GameUI currentGameUI;
+
+    [SerializeField]
+    private List<PopUpMenu> screens;
+
+    private PopUpMenu currentScreen;
+
+    public void OpenScreen(string name)
+    {
+        Enum.TryParse(name, out currentGameUI);
+        currentScreen = screens.Find(x => x.gameObject.name.Equals(currentGameUI.ToString()));
+        AnimateUI();
+    }
+
+    private void AnimateUI()
+    {
+        if (currentScreen)
+        {
+            AnimateUIHandle();
+        }
+    }
+
+    private void AnimateUIHandle()
+    {
+        for (int i = 0; i < screens.Count; ++i)
+        {
+            if (screens[i].Equals(currentScreen))
+            {
+                screens[i].TogglePopUpMenu();
+            } else
+            {
+                screens[i].HidePopUpMenu();
+            }
+        }
+    }
+}
