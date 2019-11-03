70858da0a6d98208950809f01e6fbc668a8514a1
kamcio819 <kamcio819@gmail.com>
2019-11-03 16:29:57 +0100

    Added minimap cursor range

---
+++ Assets/CursorInput.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class CursorInput : MonoBehaviour,
+    IPointerUpHandler,
+    IPointerDownHandler
+{
+    [SerializeField]
+    private RectTransform cusorRange;
+
+    [SerializeField]
+    private RectTransform miniMap;
+
+    private bool mouseOver;
+    private Vector2 pos;
+
+    private Camera cam;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        mouseOver = true;
+        cam = eventData.enterEventCamera;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        mouseOver = false;
+    }
+
+    private void Update()
+    {
+        if(mouseOver && MouseInMinimap(Input.mousePosition))
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(miniMap, Input.mousePosition, cam, out pos);
+            cusorRange.localPosition = pos;
+        }
+    }
+
+    private bool MouseInMinimap(Vector3 mousePosition)
+    {
+        if (mousePosition.x > 1600 && mousePosition.x < 1920 && mousePosition.y < 250 && mousePosition.y > 0)
+            return true;
+        else
+            return false;
+    }
+}
