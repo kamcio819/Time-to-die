9c179e623a3f86cec1cf85ab4c72ffc1b6c27fe8
kamcio819 <kamcio819@gmail.com>
2019-11-11 23:24:19 +0100

    Added materials module system script

--- Assets/Scripts/Systems/MaterialsModuleSystem.cs
+++ Assets/Scripts/Systems/MaterialsModuleSystem.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class MaterialsModuleSystem : ITEModuleSystem
 {
+    [System.Serializable]
+    public struct MaterialsData
+    {
+        [SerializeField]
+        [Range(0, 100)]
+        private int gold;
+
+        [SerializeField]
+        [Range(0, 100)]
+        private int oil;
+
+        [SerializeField]
+        [Range(0, 100)]
+        private int iron;
+
+        public int GetGold() { return gold; }
+        public int GetOil() { return oil; }
+        public int GetIron() { return iron; }
+
+        public void AddGold(int count) { gold += count; }
+        public void AddOil(int count) { oil += count; }
+        public void AddIron(int count) { iron += count; }
+    }
+
+    [SerializeField]
+    private MaterialsData materialsData;
+
     public override void Exit()
     {
     }
