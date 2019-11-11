29e51ab4d4c2f51ded8226db7c76f5fe9c582d2c
kamcio819 <kamcio819@gmail.com>
2019-11-11 22:47:28 +0100

    Added new systems

--- Assets/Scripts/ShipData.cs
+++ Assets/Scripts/ShipData.cs
@@ -2,29 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-struct Data
-{
-    [SerializeField]
-    [Range(0, 50f)]
-    float damage;
-
-    [SerializeField]
-    [Range(1, 4)]
-    int moveRange;
-
-    [SerializeField]
-    [Range(0, 8f)]
-    float shootingRange;
-}
 
 [CreateAssetMenu(fileName = "ShipData", menuName = "Ships/Data", order = 1)]
 [PreferBinarySerialization]
 public class ShipData :  ScriptableObject
 {
+    [System.Serializable]
+    public struct Data
+    {
+        [SerializeField]
+        [Range(0, 200f)]
+        float maxHealth;
+
+        [SerializeField]
+        [Range(0, 50f)]
+        float damage;
+
+        [SerializeField]
+        [Range(1, 4)]
+        int moveRange;
+
+        [SerializeField]
+        [Range(0, 8f)]
+        float shootingRange;
+
+        public float GetDamage() { return damage; }
+        public int GetMovementRange() { return moveRange; }
+        public float GetAttackRange() { return shootingRange; }
+        public float GetMaxHealth() { return maxHealth; }
+
+        public void AddDamage(float _damage) { damage += _damage; }
+        public void AddMovementRange(int _moveRange) { moveRange += _moveRange; }
+        public void AddAttackRange(float _shootingRange) { shootingRange += _shootingRange; }
+        public void AddMaxHealth(float _maxHealth) { maxHealth += _maxHealth; }
+    }
+
+
     [SerializeField]
-    Data shipData;
+    private Data shipData;
 
     [SerializeField]
     private ParticleSystem damageParticle;
+
+    public Data ShipDataContainer { get => shipData; set => shipData = value; }
 }
---
+++ Assets/ShipDataUpgrader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipDataUpgrader : MonoBehaviour
+{
+    [SerializeField]
+    private List<ShipData> shipsData;
+
+    public void UpgradeHealth()
+    {
+        ShipData sd = shipsData[Random.Range(0, shipsData.Count - 1)];
+        sd.ShipDataContainer.AddMaxHealth(10f);
+    }
+
+    public void UpgradeMove()
+    {
+        ShipData sd = shipsData[Random.Range(0, shipsData.Count - 1)];
+        sd.ShipDataContainer.AddMovementRange(1);
+    }
+
+    public void UpgradeDamage()
+    {
+        ShipData sd = shipsData[Random.Range(0, shipsData.Count - 1)];
+        sd.ShipDataContainer.AddDamage(5f);
+    }
+}
