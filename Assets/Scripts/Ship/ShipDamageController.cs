ba78321cd561963e5111e077fd124131de1ac218
kamcio819 <kamcio819@gmail.com>
2019-11-30 22:00:52 +0100

    Added health damage visualization for ships

--- Assets/Scripts/Ship/ShipDamageController.cs
+++ Assets/Scripts/Ship/ShipDamageController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,13 +12,47 @@ public class ShipDamageController : MonoBehaviour
     [SerializeField]
     private Image healthBar = default;
 
+    [SerializeField]
+    private MeshRenderer meshRenderer = default;
+
+    private Material[] objectMaterials;
+    private Color color;
+
+    private void Awake()
+    {
+        objectMaterials = meshRenderer.materials;
+    }
+
     public void DamageShip(float damage)
     {
         shipDataController.health -= damage;
         healthBar.fillAmount = shipDataController.health / 100f;
+        StartCoroutine(GiveDamageVisual());
         if (shipDataController.health <= 0f)
         {
             Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator GiveDamageVisual()
+    {
+        for (float i = 0f; i < 1.5f; i += Time.deltaTime)
+        {
+            ToggleMaterials(0f);
+            yield return null;
+            ToggleMaterials(1f);
+            yield return null;
+        }
+        ToggleMaterials(1f);
+    }
+
+    private void ToggleMaterials(float val)
+    {
+        for (int j = 0; j < objectMaterials.Length; ++j)
+        {
+            color = objectMaterials[j].color;
+            color.a = val;
+            objectMaterials[j].color = color;
         }
     }
 }
